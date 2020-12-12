3eed9b346d570efe5ea4b3ab05f6584823e987f3
p.pandi <p.pandi@napoleongames.be>
2020-12-12 02:29:45 +0100

    Updating the template and ReadMe

--- README.md
+++ README.md
@@ -8,3 +8,22 @@ Supermarket
 ## Running PHP Unit Tests
 
 ```php bin/phpunit tests/```
+
+## To view the price calculation
+
+```http://127.0.0.1:8000/checkout/review```
+
+## To test the code by changing no of items
+
+```src/Controller/ItemController.php```
+
+In the above path change the number of items in the constant
+
+```    const NO_OF_ITEMS_A = 7;
+       const NO_OF_ITEMS_B = 4;
+       const NO_OF_ITEMS_C = 7;
+       const NO_OF_ITEMS_D = 8;
+       const NO_OF_ITEMS_E = 5;
+```
+
+And refresh the page checkout/review to see new results.
--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -15,6 +15,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ItemController extends AbstractController
 {
+    const NO_OF_ITEMS_A = 4;
+    const NO_OF_ITEMS_B = 4;
+    const NO_OF_ITEMS_C = 6;
+    const NO_OF_ITEMS_D = 9;
+    const NO_OF_ITEMS_E = 6;
+
     public function __construct(ItemService $itemService, Checkout $checkoutService)
     {
         $this->itemService = $itemService;
@@ -32,11 +38,11 @@ class ItemController extends AbstractController
         $itemD = new ItemModel('D', 15);
         $itemE = new ItemModel('E', 5);
 
-        $addCart1 = new CartItem($itemA, 7);
-        $addCart2 = new CartItem($itemB, 4);
-        $addCart3 = new CartItem($itemC, 7);
-        $addCart4 = new CartItem($itemD, 8);
-        $addCart5 = new CartItem($itemE, 5);
+        $addCart1 = new CartItem($itemA, self::NO_OF_ITEMS_A);
+        $addCart2 = new CartItem($itemB, self::NO_OF_ITEMS_B);
+        $addCart3 = new CartItem($itemC, self::NO_OF_ITEMS_C);
+        $addCart4 = new CartItem($itemD, self::NO_OF_ITEMS_D);
+        $addCart5 = new CartItem($itemE, self::NO_OF_ITEMS_E);
 
 
         $cartItems = new CartCollection([$addCart1, $addCart2, $addCart3, $addCart4, $addCart5]);
